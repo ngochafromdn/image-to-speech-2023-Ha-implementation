d6f4824ef9e01088744ba702639835f019886165
ngochafromdn <61641363+ngochafromdn@users.noreply.github.com>
2023-06-05 07:58:50 +0700

    Update read_aloud.py

--- voice/read_aloud.py
+++ voice/read_aloud.py
@@ -1,5 +1,6 @@
 from gtts import gTTS
 import pygame
+from pygame import mixer
 import tempfile
 
 class ReadingWord:
@@ -8,7 +9,7 @@ class ReadingWord:
         self.engine = None
         
         # Initialize pygame mixer
-        pygame.mixer.init()
+        mixer.init()
 
     def read_aloud(self, word, lang='en'):
         tts = gTTS(text=word, lang=lang)
@@ -17,9 +18,9 @@ class ReadingWord:
             tts.save(fp.name + ".mp3")
             
             # Load and play the audio using pygame mixer
-            pygame.mixer.music.load(fp.name + ".mp3")
-            pygame.mixer.music.play()
+            mixer.music.load(fp.name + ".mp3")
+            mixer.music.play()
             
             # Wait until the audio finishes playing
-            while pygame.mixer.music.get_busy():
+            while mixer.music.get_busy():
                 pass
